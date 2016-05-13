fe3834669ab6f545278f18adfbfed2851bddae9c
rranjan <ritweek@gmail.com>
2016-05-13 17:55:22 +0530

    WCF

---
+++ SingleSignOn/Models/UserDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SingleSignOn.Models
+{
+    public class UserDetails
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string UserName { get; set; }
+        [Required]
+    [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ SingleSignOn/Service/Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using SingleSignOn.Models;
+
+namespace SingleSignOn.Service
+{
+    public class Context : DbContext
+    {
+
+        public Context() : base  ("Context")
+        {
+           
+        }
+
+        public DbSet<UserDetails> Users { get; set; } 
+    }
+}
